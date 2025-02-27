746e7fab3df36127c6747ad4766fe77359b5d93f
anveshdandala <anveshdandala@gmail.com>
2025-02-27 21:18:39 +0530

    commeted code

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,17 @@ import FrontPage1Right from "./components/FrontPage1Right";
 import FrontPage1Nav from "./components/FrontPage1Nav";
 import Customer from "./components/Customer/pages/Customer"; // Import Customer page
 import "./FrontPage1.css";
-
+//can also use Outlets to render the components
+// import { Outlet } from "react-router-dom";
+// use link to navigate to the page without refreshing the page
+// import { Link } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Home Page */}
         <Route
+          exact
           path="/"
           element={
             <section className="container-root">
